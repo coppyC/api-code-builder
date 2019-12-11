9e93fd007811074e1181a5c68a3906fd8eeec242
coppy <zheqingli@icloud.com>
2019-12-11 14:58:52 +0800

    支持 basic 认证

--- bin/request.js
+++ bin/request.js
@@ -5,14 +5,14 @@ module.exports = function request(url, author) {
   let client = http
   if (/^https/.test(url)) client = https
 
+  const headers = {
+    'user-agent': 'https://github.com/coppyC/api-code-builder',
+  }
+  if (author)
+    headers.Authorization = `Basic ${Buffer.from(`${author.username}:${author.password}`).toString('base64')}`
+
   return new Promise((resolve, reject) => {
-    console.log(`Basic ${Buffer.from(`${author.username}:${author.password}`).toString('base64')}`)
-    client.get(url, {
-      headers: {
-        'user-agent': 'https://github.com/coppyC/api-code-builder',
-        'Authorization': author ? `Basic ${Buffer.from(`${author.username}:${author.password}`).toString('base64')}` :undefined
-      }
-    }, res => {
+    client.get(url, { headers }, res => {
       let data = ''
       const logError = () => {
         console.error(`url: ${url}\n[statusCode: ${res.statusCode}] ${data}`)
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,8 @@
 
-export const upperFirstCase = (name: string) =>
-  name[0].toUpperCase() + name.slice(1)
+export const upperFirstCase = (name: string) => {
+  if (!name || !name.length) return ''
+  return name[0].toUpperCase() + name.slice(1)
+}
 
 export const lintVariable = (name: string) =>
   name.replace(/[^\w]/g, '')
